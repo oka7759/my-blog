e496c3739b892ff9dd66ef64e6591306ad32c660
억화 <oka@eoghwaui-Macmini.local>
2025-02-04 22:33:18 +0900

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,169 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+export function Top() {
+  return (
+    <div className={styles.top}>
+      <div className={styles.topTitleBox}>
+        <h1>Blog</h1>
+        <div className={styles.topSubTitleBox}>
+          <p>개발하면서 탐구한 것을 소소하게 기록하는 공간입니다.</p>
+          <p>
+            시리즈로 연재된 포스트는 아래 시리즈북으로 편리하게 열람할 수
+            있습니다🧐
+          </p>
+        </div>
+        <div className={styles.searchBox}>
+          <input></input>
+        </div>
+        <div className={styles.bookBox}>
+          <div className={styles.book}>
+            <div className={styles.bookLineFirst}></div>
+            <div className={styles.bookLineSecond}></div>
+            <div className={styles.face}>BOOK!</div>
+          </div>
+          <div className={styles.book}>
+            <div className={styles.bookLineFirst}></div>
+            <div className={styles.bookLineSecond}></div>
+            <div className={styles.face}>BOOK!</div>
+          </div>
+          <div className={styles.book}>
+            <div className={styles.bookLineFirst}></div>
+            <div className={styles.bookLineSecond}></div>
+            <div className={styles.face}>BOOK!</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
+export function Buttom() {
+  return (
+    <div className={styles.bottom}>
+      <div className={styles.topTitleBox}>
+        <h1>All Posts</h1>
+      </div>
+      <div className={styles.postList}>
+        <div className={styles.post}>
+          <h2>블로그에 RSS, JSON Feed 추가하기</h2>
+          <p>안녕하세요 제 블로그 글입니다.</p>
+          <div className={styles.postInfo}>
+            <div className={styles.tags}>
+              <span>css</span>
+              <span>nextjs</span>
+            </div>
+            <div className={styles.date}>
+              <span>23.0..29</span>
+              <span>10 ago</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.post}>
+          <h2>블로그에 RSS, JSON Feed 추가하기</h2>
+          <p>안녕하세요 제 블로그 글입니다.</p>
+          <div className={styles.postInfo}>
+            <div className={styles.tags}>
+              <span>css</span>
+              <span>nextjs</span>
+            </div>
+            <div className={styles.date}>
+              <span>23.0..29</span>
+              <span>10 ago</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.post}>
+          <h2>블로그에 RSS, JSON Feed 추가하기</h2>
+          <p>안녕하세요 제 블로그 글입니다.</p>
+          <div className={styles.postInfo}>
+            <div className={styles.tags}>
+              <span>css</span>
+              <span>nextjs</span>
+            </div>
+            <div className={styles.date}>
+              <span>23.0..29</span>
+              <span>10 ago</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.post}>
+          <h2>블로그에 RSS, JSON Feed 추가하기</h2>
+          <p>안녕하세요 제 블로그 글입니다.</p>
+          <div className={styles.postInfo}>
+            <div className={styles.tags}>
+              <span>css</span>
+              <span>nextjs</span>
+            </div>
+            <div className={styles.date}>
+              <span>23.0..29</span>
+              <span>10 ago</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.post}>
+          <h2>블로그에 RSS, JSON Feed 추가하기</h2>
+          <p>안녕하세요 제 블로그 글입니다.</p>
+          <div className={styles.postInfo}>
+            <div className={styles.tags}>
+              <span>css</span>
+              <span>nextjs</span>
+            </div>
+            <div className={styles.date}>
+              <span>23.0..29</span>
+              <span>10 ago</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.post}>
+          <h2>블로그에 RSS, JSON Feed 추가하기</h2>
+          <p>안녕하세요 제 블로그 글입니다.</p>
+          <div className={styles.postInfo}>
+            <div className={styles.tags}>
+              <span>css</span>
+              <span>nextjs</span>
+            </div>
+            <div className={styles.date}>
+              <span>23.0..29</span>
+              <span>10 ago</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.post}>
+          <h2>블로그에 RSS, JSON Feed 추가하기</h2>
+          <p>안녕하세요 제 블로그 글입니다.</p>
+          <div className={styles.postInfo}>
+            <div className={styles.tags}>
+              <span>css</span>
+              <span>nextjs</span>
+            </div>
+            <div className={styles.date}>
+              <span>23.0..29</span>
+              <span>10 ago</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.post}>
+          <h2>블로그에 RSS, JSON Feed 추가하기</h2>
+          <p>안녕하세요 제 블로그 글입니다.</p>
+          <div className={styles.postInfo}>
+            <div className={styles.tags}>
+              <span>css</span>
+              <span>nextjs</span>
+            </div>
+            <div className={styles.date}>
+              <span>23.0..29</span>
+              <span>10 ago</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <>
+      <Top />
+      <Buttom />
+    </>
   );
 }
